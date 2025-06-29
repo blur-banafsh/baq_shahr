a7277dc375e17307eb816aaa84038105e702eb38
unknown <blurbanafsh@gmail.com>
2025-06-29 19:50:13 +0330

    'debug'

--- baqeshahr/settings.py
+++ baqeshahr/settings.py
@@ -23,12 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8fn_x#yhl_!a$rat_0#emor(k^8z(r1rkjjx=syfl7kb2chr0('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
